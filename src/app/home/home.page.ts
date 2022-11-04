57a1cedb6eace04f07da8c3addb3c5c98d6d6d5d
Krishna <kri.vri1989@gmail.com>
2022-11-04 17:35:36 +0530

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+
+// get token
+function generateToken(
+  tokenServerUrl: string,
+  userID: string,
+  roomID: string,
+  userName: string
+) {
+  // Obtain the token interface provided by the App Server
+  return fetch(
+    `${tokenServerUrl}/access_token?userID=${userID}&userName=${userName}&roomID=${roomID}&expired_ts=7200`,
+    {
+      method: 'GET',
+    }
+  ).then((res) => res.json());
+}
+
+function randomID(len) {
+  let result = '';
+  if (result) return result;
+  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+    maxPos = chars.length,
+    i;
+  len = len || 5;
+  for (i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
+}
+
+export function getUrlParams(
+  url: string = window.location.href
+): URLSearchParams {
+  let urlStr = url.split('?')[1];
+  return new URLSearchParams(urlStr);
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +43,44 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  @ViewChild('root')
+  root: ElementRef;
+
+  ngAfterViewInit() {
+    const roomID = getUrlParams().get('roomID') || randomID(5);
+
+    // generate Kit Token
+    const appID = 1251025811;
+    const serverSecret = '';
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      randomID(5),
+      randomID(5)
+    );
+
+    // Create instance object from Kit Token.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    // Start a call.
+    zp.joinRoom({
+      container: this.root.nativeElement,
+      sharedLinks: [
+        {
+          name: 'Personal link',
+          url:
+            window.location.origin +
+            window.location.pathname +
+            '?roomID=' +
+            roomID,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+      },
+    });
+  }
 }
